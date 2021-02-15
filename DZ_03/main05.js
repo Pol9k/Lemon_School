82e0c630cc1e9763c12f72cbed54d3ad437ec8df
AndriyB <you@example.com>
2021-02-15 19:05:11 +0200

    DZ_03 01,02,03,04,05

--- DZ_03/main05.js
+++ DZ_03/main05.js
@@ -2,12 +2,12 @@
 Найти номер столбца двумерного массива сумма которого является максимальной (аналогично для строки)
 */
 let m = [[1,2,3],[4,5,6],[7,8,9]];
-let maxColumnSum = function(){
+let maxColumnSum = function(arr){
     let rez = 0;
     let sum = [];
-    for(let i = 0 ; i < m.length; i++){
-        for(let j = 0; j < m[i].length; j++){
-            rez += m[i][j]
+    for(let i = 0 ; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            rez += arr[i][j];
         }
         sum.push(rez);
     }
